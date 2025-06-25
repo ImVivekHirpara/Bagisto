d7d91f67575a6881a20583d4bccf591382308080
Vivek <vivekhirpara5346@gmail.com>
2025-06-25 15:47:17 +0530

    Pincode check  button update

--- packages/Webkul/Shop/src/Resources/views/products/view.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view.blade.php
@@ -388,7 +388,7 @@
                                             <button 
                                                 type="button" 
                                                 @click="checkPincode" 
-                                                class="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-blue-600"
+                                                class="px-6 py-3 bg-blue-600 text-black font-medium rounded-lg border-2 border-blue-700 hover:bg-blue-700 hover:border-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-blue-600 disabled:hover:border-blue-700"
                                                 :disabled="pincode.length !== 6 || isCheckingPincode"
                                             >
                                                 <span v-if="!isCheckingPincode" class="flex items-center gap-2">
